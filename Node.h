9353db92f64d2c14551534cb232dcc722e03dd99
hermesonbastos <hermesonbastos@gmail.com>
2023-05-30 10:54:59 -0300

    Classe Node.h finalizada

--- Node.h
+++ Node.h
@@ -1,27 +1,39 @@
-// classe Node.h criada em 29/05 por Hermeson Bastos
-/*
-    Pontos importantes:
-        - A estrutra do projeto foi criada com base nas atividades anteriores de listas simplesmente encadeadas e na motivação do problema disponibilizado pelo professor no Moodle.
-*/
+//--------------------------- Dupla ---------------------------//
+//                                                             //
+//                Hermeson Bastos Maia - 537806                //
+//             João Paulo Almeida Miranda - 540923
+//                                                             //
+//-------------------------------------------------------------//
+
+
+
+// > Classe Node.h criada em 29/05 por Hermeson Bastos
+//
+// PONTOS INPORTANTES:
+//      - A estrutra do projeto foi criada com base nas
+//        atividades anteriores de listas simplesmente 
+//        encadeadas e na motivação do problema         
+//        disponibilizado pelo professor no Moodle.
+
 
 #ifndef NODE_H
 #define NODE_H
-
-using Item = double;
-
 class Node {
 
     private:
-        Node *direita;
-        Node *abaixo;
-        int linha;
-        int coluna;
-        Item valor;
+        Node *direita; // ponteiro para o nó à direita
+        Node *abaixo; // ponteiro para o nó abaixo
+        int linha; // linha de referência
+        int coluna; // coluna de referência
+        double valor; // valor guardado no nó
+
     public:
-        Node(const Item& val, Node *right = nullptr, Node *down = nullptr){
+        Node(const double& val, Node *right = nullptr, Node *down = nullptr, int l, int c){
             valor = val;
             direita = right;
             abaixo = down;
+            linha = l;
+            coluna = c;
         };
 };
 
