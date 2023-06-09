971a94fba606e1ae362615b9fa6ce2a73f77c478
hermesonbastos <hermesonbastos@gmail.com>
2023-06-09 19:27:48 -0300

    erros do insert corrigidos: linhas e colunas invertidas e problema ao inserir na última coluna

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -26,29 +26,33 @@ class SparseMatrix {
         // - Recebe o número de linhas(m) e colunas(n)
         SparseMatrix(int m, int n){
             if(m > 0 && n > 0){
-                m_head = new Node(nullptr, nullptr, 0, 0); // cria o nó cabeça apontado para nulo
-                n_col += m; // incrementa o número de linhas
-                n_lin += n; // incrementa o número de colunas
+                m_head = new Node(m_head, m_head, 0, 0); // cria o nó cabeça apontado para ele mesmo para e baixo e para a direita
+                n_lin = m; // incrementa o número de linhas
+                n_col = n; // incrementa o número de colunas
                 
-                Node *aux = m_head;
-                for(int i = 1; i <= n_lin; i++){
-                    aux->abaixo = new Node(nullptr, nullptr, i, 0);
-                    aux = aux->abaixo;
-                    aux->direita = aux;
+                Node *aux = m_head; // auxiliar é o m_head
+                for(int i = 1; i <= n_lin; i++){ // percorre o número de linhas de 1 até o tamanho
+                    aux->abaixo = new Node(nullptr, nullptr, i, 0); // auxiliar abaixo aponta para um novo nó
+                    aux = aux->abaixo; // auxiliar é o nó abaixo dele
+                    aux->direita = aux; // auxiliar direita aponta para ele mesmo
+                    aux->abaixo = m_head;
                 }
-                aux->abaixo = m_head;
 
-                aux = m_head;
-                for(int i = 1; i <= n_lin; i++){
-                    aux->direita = new Node(nullptr, nullptr, 0, i);
-                    aux = aux->direita;
-                    aux->abaixo = aux;
+                aux = m_head; // auxiliar é o m_head
+                for(int i = 1; i <= n_col; i++){ // percorre o número de colunas, do 1 ao tamanho
+                    aux->direita = new Node(nullptr, nullptr, 0, i); // auxiliar direita aponta para um NOVO nó
+                    aux = aux->direita; // auxiliar é o NOVO nó
+                    aux->abaixo = aux; // auxiliar abaixo aponta para ele mesmo
+                    aux->direita = m_head;
                 }
-                aux->direita = m_head;
             } else {
                 cout << "Insira tamanhos válidos! (O número de linhas e de colunas devem ser maiores que 0)." << endl;
             }
         };
+
+        bool indice_invalido(int i, int j){
+            return i > n_lin || j > n_col || i < 1 || j < 1;
+        }
 
         // Destrutor
         ~SparseMatrix();
@@ -57,37 +61,38 @@ class SparseMatrix {
         // - Recebe a linha(i) e a coluna (j) junto do valor(value) a ser inserido na determinada posição
         void insert(int i, int j, double value){
 
-            if(i > this->n_lin || j > this->n_col || i < 1 || j < 1){
-                cout << "Insira índices válidos! (Correspondentes ao tamanho da matriz informada)." << endl;
+            if(indice_invalido(i, j)){
+                cout << "insercao no indice errado" << endl;
+                return;
             } else {
 
-                Node *auxl = m_head;
-                for(int k = 1; k <= n_lin; k++){
+                Node *auxl = m_head; // auxl é m_head
+                for(int k = 1; k <= n_lin; k++){ // percorre todas as linhas, de 1 até o número de linhas
                     if(auxl->linha == i){
                         break;
                     } else {
-                        auxl = auxl->abaixo;
+                        auxl = auxl->abaixo; // auxl é o próximo nó abaixo
                     }
                 }
 
-                Node *auxc = m_head;
-                for(int k = 1; k <= n_col; k++){
+                Node *auxc = m_head; // aux é m_head
+                for(int k = 1; k <= n_col; k++){ // percorre todas as colunas, de 1 ao tamanho
                     if(auxc->coluna == j){
                         break;
                     } else {
-                        auxc = auxc->direita;
+                        auxc = auxc->direita; // auxc é igual ao próximo a direita
                     }
                 }
 
                 if(auxl->direita == auxl){ // quando a linha está vazia
-                    if(auxc->abaixo == auxc){ // e quando a coluna
+                    if(auxc->abaixo == auxc){ // quando a coluna está vazia
                         auxl->direita = new Node(value, auxl, auxc, i, j);
                         auxc->abaixo = auxl->direita;
                     } else {
-                        Node *atual = auxc;
-                        for(int at = 0; at <= n_lin; at++){
+                        Node *atual = auxc; // atual é auxc
+                        for(int at = 0; at <= n_lin; at++){ // percorre da coluna sentinela atée o último nó da coluna,ou seja do 0 ao tamanho
                             if((i > atual->linha && i < atual->abaixo->linha) || atual->abaixo->linha == 0){
-                                Node* novo = new Node(value, atual->abaixo, auxl, i, j);
+                                Node* novo = new Node(value, auxl, atual->abaixo, i, j);
                                 auxl->direita = novo;
                                 atual->abaixo = novo;
                                 break;
@@ -99,7 +104,7 @@ class SparseMatrix {
                 } else { // quando a linha possui pelo menos um nó
                     if(auxc->abaixo == auxc){
                         Node *atual = auxl;
-                        for(int at = 0; at <= n_col; at++){
+                        for(int at = 0; at <= n_col; at++){ // percorre o sentinela ao último nó, ou seja, de 0 ao tamanho
                             if((j > atual->coluna && j < atual->direita->coluna) || atual->direita->coluna == 0){
                                 Node* novo = new Node(value, atual->direita, auxc, i, j);
                                 auxc->abaixo = novo;
@@ -110,60 +115,99 @@ class SparseMatrix {
                             }
                         }
                     } else {
-                        Node *atual_linha = auxl;
-                        Node *anterior = nullptr;
-                        for(int at = 0; at <= n_col; at++){
-                            if(atual_linha->coluna < j){
-                                anterior = atual_linha;
-                                atual_linha = atual_linha->direita;
-                            } else if(atual_linha->coluna == j){
-                                atual_linha->valor = value;
-                                break;
-                            } else { 
-                                Node *atual_coluna = auxc;
-                                for(int at = 0; at <= i; at++){
-                                    if(atual_coluna->abaixo->linha < i){ // se o nó após o atual_coluna é menor do que i
-                                        atual_coluna = atual_coluna->abaixo;
-                                    } else {
-                                        break;
-                                    }
-                                }
+                        Node *atual_linha = auxl->direita; // começa no primeiro nó da linha
+                        Node *anterior = auxl; // o nó anterior é o sentinela
+                        bool encontrouColuna = false;
 
-                                Node *novo = new Node(value, atual_linha, atual_coluna->abaixo, i, j);
+                        while (atual_linha != auxl) { // percorre os nós da linha até voltar ao sentinela
+                            if (atual_linha->coluna == j) { // nó com a coluna encontrada
+                                atual_linha->valor = value;
+                                encontrouColuna = true;
+                                break;
+                            } else if (atual_linha->coluna > j) { // insere antes do nó atual
+                                Node *novo = new Node(value, atual_linha, atual_linha->abaixo, i, j);
                                 anterior->direita = novo;
-                                atual_coluna->abaixo = novo;
+                                atual_linha->abaixo = novo;
+                                encontrouColuna = true;
+                                break;
                             }
+                            anterior = atual_linha;
+                            atual_linha = atual_linha->direita;
+                        }
+
+                        if (!encontrouColuna) { // insere no final da linha
+                            Node *novo = new Node(value, auxl, auxc, i, j);
+                            anterior->direita = novo;
+                            auxc->abaixo = novo;
                         }
                     }
                 }
 
-                // quando o nó já existe
-                
-                // não há nada na linha nem na coluna
-                // if(auxl->direita == auxl && auxc->abaixo == auxc){
-                //     auxl->direita = new Node(value, auxl, auxc, i, j);
-                //     auxc->abaixo = auxl->direita;
-                // } else {
-            
-                // }
-
-                // // quando há nós na linha mas não há nas colunas
-                // if(auxl->direita != auxl && auxc->abaixo == auxc){
-                //     Node *atual = auxl;
-                //     for(int at = 1; at <= n_col; at++){
-                //         atual = atual->direita;
-                //     }
-                // }
             }
         };
 
-        // Devolve o valor na célula (i, j) da matriz
-        // Checa se os índices passados são válidos
-        double get(int i, int j);
+        double get(int i, int j){
+            if (indice_invalido(i, j)) {
+                cout << "impressao no indice errado" << endl;
+                return 0.0;
+            } else {
+                Node* auxl = m_head;
+                for (int k = 1; k <= n_lin; k++) {
+                    if (auxl->linha == i) {
+                        break;
+                    } else {
+                        auxl = auxl->abaixo;
+                    }
+                }
+
+                Node* auxc = m_head;
+                for (int k = 1; k <= n_col; k++) {
+                    if (auxc->coluna == j) {
+                        break;
+                    } else {
+                        auxc = auxc->direita;
+                    }
+                }
+
+                Node* atual_linha = auxl;
+                for (int at = 0; at <= n_col; at++) {
+                    if (atual_linha->coluna < j) {
+                        atual_linha = atual_linha->direita;
+                    } else if (atual_linha->coluna == j) {
+                        return atual_linha->valor;
+                    } else {
+                        return 0.0; // Elemento não encontrado, valor padrão 0.0
+                    }
+                }
+            }
+
+            return 0.0; // Elemento não encontrado, valor padrão 0.0
+        };
+
 
         // Printa a matriz A no terminal
         // Inclusive os elementos iguais a 0
-        void print();
+        void print(){
+            Node* auxl = m_head->abaixo; // First auxl (excluding the head node)
+
+            // Traverse each auxl
+            while (auxl != m_head) {
+            Node* atual = auxl->direita; // First node in the auxl (excluding the head node)
+
+             // Traverse each node in the auxl
+            for (int j = 1; j <= n_col; j++) {
+                if (atual != auxl && atual->coluna == j) {
+                    cout << atual->valor << " ";
+                    atual = atual->direita;
+                } else {
+                    cout << "0 ";
+                }
+            }   
+
+            cout << endl;
+            auxl = auxl->abaixo;
+            }
+        };
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //--------------------------- Dupla ---------------------------//
 //                                                             //
 //                Hermeson Bastos Maia - 537806                //
-//             João Paulo Almeida Miranda - 540923
+//             João Paulo Almeida Miranda - 540923             //
 //                                                             //
 //-------------------------------------------------------------//
 
@@ -10,11 +10,80 @@
 #include <fstream>
 #include <vector>
 
-int main(){
-    SparseMatrix *matriz = new SparseMatrix(5, 5);
-    
-    matriz->insert(1, 1, 13.5);
-    matriz->insert(2, 4, 0);
-    matriz->insert(5, 3, 13.5);
-    matriz->insert(5,3, 13.5);
+SparseMatrix* readSparseMatrix(std::string matrix) {
+   std::ifstream input_arquivo("matrix.txt");
+
+   if(!input_arquivo.is_open()) {
+      std::cout << "Erro ao abrir o arquivo de entrada, tente novamente." << std::endl;
+      return nullptr;
+   }
+
+   int numLin, numCol;
+   input_arquivo >> numLin >> numCol;
+
+   SparseMatrix* read_matrix = new SparseMatrix(numLin, numCol);
+
+   int lin, col, value;
+   while (input_arquivo >> lin >> col >> value) {
+      read_matrix->insert(lin, col, value);
+   }
+   
+   input_arquivo.close();
+
+   return read_matrix;
+}
+
+int main() {
+   std::vector<SparseMatrix*> vec_matrix;
+   std::string arquivo_matriz = "matrix.txt";
+
+   
+   int resposta;
+   while (resposta != 4) {
+      std::cout << "\n ----- Selecione uma das alternativas abaixo -----\n";
+      std::cout << "1. Criar uma matriz\n";
+      std::cout << "2. Inserir elementos na sua matriz\n";
+      std::cout << "3. Imprimir uma matriz\n";
+      std::cout << "4. Imprimir matriz(es)\n";
+      std::cout << "5. Sair\n";
+      std::cout << "digite sua resposta: ";
+      std::cin >> resposta;
+
+      switch (resposta) {
+         case 1: {
+            int numLin, numCol;
+            cout << "\nInforme o numero de linhas da sua matriz: ";
+            cin >> numLin;
+            cout << "Agora informe o numero de colunas da sua matriz: ";
+            cin >> numCol;
+
+            if(numLin == 0 || numCol == 0) {
+               cout << "\n-- Matriz invalida, por favor tente novamente. --\n";
+               cout << "\nDica: A definicao de uma matriz requer pelo menos uma linha e uma coluna para ter algum tipo de conteudo. Uma matriz sem elementos nao faz sentido, pois nao existem dados para armazenar ou acessar.\n";
+               break;
+            }
+            SparseMatrix *sparseMatrix = new SparseMatrix(numLin, numCol);
+            vec_matrix.push_back(sparseMatrix);
+
+            cout << "\n--- Matriz criada com sucesso! ---\n";
+            break;
+         }
+         case 2: {
+            int lin, col;
+            double value;
+            std::cout << "Digite a linha que voce ira inserir: ";
+            cin >> lin;
+            std::cout << "Digite a coluna que voce ira inserir: ";
+            std::cin >> col;
+            std::cout << "Digite o valor que voce quer inserir na sua matriz:";
+            std::cin >> value;
+            vec_matrix[0]->insert(lin, col, value);
+            std::cout << "\n--- Elementos inseridos com sucesso na matriz! ---\n";
+            break;
+         }
+         case 3: {
+            vec_matrix[0]->print();
+         }
+      }
+   }
 }
