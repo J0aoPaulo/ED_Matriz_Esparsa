02a04957baebd828295365d5b58b342d117341fd
João Paulo <98539735+J0aoPaulo@users.noreply.github.com>
2023-06-10 17:05:06 -0300

    Atualizando main.cpp.
    Adicionando funcionalidades e deixando-a mais interativa.

--- main.cpp
+++ main.cpp
@@ -33,19 +33,30 @@ SparseMatrix* readSparseMatrix(std::string matrix) {
    return read_matrix;
 }
 
+std::string matrixExist(std::vector<SparseMatrix*> vector_matrix) {
+   if(vector_matrix.empty()) {
+      throw std::runtime_error("\n*Nao foi encontrada nenhuma matriz para ser manipulada.*");
+   }
+   return "\n--- Verificando se matriz existe... ---\n\n--- Matriz verificada e existente! ---\n";
+}
+
 int main() {
+   // desalocar endereços de memoria
+   // complexidade relatorio insert
    std::vector<SparseMatrix*> vec_matrix;
    std::string arquivo_matriz = "matrix.txt";
-
+   int it = 0; // pode melhorar 
    
    int resposta;
    while (resposta != 4) {
+      std::cout << "\n ------ Menu de criacao e manipulacao de Matriz Esparsa ------\n";
       std::cout << "\n ----- Selecione uma das alternativas abaixo -----\n";
-      std::cout << "1. Criar uma matriz\n";
-      std::cout << "2. Inserir elementos na sua matriz\n";
-      std::cout << "3. Imprimir uma matriz\n";
-      std::cout << "4. Imprimir matriz(es)\n";
-      std::cout << "5. Sair\n";
+      std::cout << "1. Criar uma matriz.\n";
+      std::cout << "2. Inserir elementos na sua matriz.\n";
+      std::cout << "3. Imprimir matriz(es).\n";
+      std::cout << "4. Somar matrizes. *em andamento*\n";
+      std::cout << "5. Multiplicar matrizes. *em andamento*\n";
+      std::cout << "6. Sair. *digite 4 para sair*\n";
       std::cout << "digite sua resposta: ";
       std::cin >> resposta;
 
@@ -64,49 +75,72 @@ int main() {
             }
             SparseMatrix *sparseMatrix = new SparseMatrix(numLin, numCol);
             vec_matrix.push_back(sparseMatrix);
+            delete sparseMatrix;
 
             cout << "\n--- Matriz criada com sucesso! ---\n";
             break;
          }
          case 2: {
+            try { // captura de exceção
+               std::cout << matrixExist(vec_matrix);
+            } catch(const std::runtime_error& e) {
+               std::cout << e.what() << std::endl; // descricao da exceção
+               break; // Interrompe o case 3 se a matriz não existir
+            }
+
             int lin, col;
             double value;
-            std::cout << "Digite a linha que voce ira inserir: ";
+            std::cout << "\nDigite a linha que voce ira inserir: ";
             cin >> lin;
             std::cout << "Digite a coluna que voce ira inserir: ";
             std::cin >> col;
-            std::cout << "Digite o valor que voce quer inserir na sua matriz:";
+            std::cout << "Digite o valor que voce quer inserir na sua matriz: ";
             std::cin >> value;
-            vec_matrix[0]->insert(lin, col, value);
+            vec_matrix[it]->insert(lin, col, value);
             std::cout << "\n--- Elementos inseridos com sucesso na matriz! ---\n";
+            it++; // pode ser melhorado || quebra galho
             break;
          }
          case 3: {
-            vec_matrix[0]->insert(3, 3, 9); //
-            vec_matrix[0]->insert(2, 4, 9); //
-            vec_matrix[0]->insert(1, 3, 9); //
-            vec_matrix[0]->insert(1, 1, 9); //
-            vec_matrix[0]->insert(1, 2, 9); //
-            vec_matrix[0]->insert(2, 3, 0); //
-            vec_matrix[0]->insert(3, 4, 9); //
-            vec_matrix[0]->insert(3, 2, 9); //
-            vec_matrix[0]->insert(1, 4, 9); //
-            vec_matrix[0]->insert(2, 1, 9); //
-            vec_matrix[0]->insert(2, 2, 9); //
-            vec_matrix[0]->insert(3, 1, 9); //
+            try { // captura de exceção
+               std::cout << matrixExist(vec_matrix);
+            } catch(const std::runtime_error& e) {
+               std::cout << e.what() << std::endl; // descricao da exceção
+               break; // Interrompe o case 3 se a matriz não existir
+            }
 
-            cout << vec_matrix[0]->get(1, 1);
-            cout << vec_matrix[0]->get(1, 2); //
-            cout << vec_matrix[0]->get(1, 3); //
-            cout << vec_matrix[0]->get(1, 4) << endl; //
-            cout << vec_matrix[0]->get(2, 1); //
-            cout << vec_matrix[0]->get(2, 2); //
-            cout << vec_matrix[0]->get(2, 3);
-            cout << vec_matrix[0]->get(2, 4) << endl;
-            cout << vec_matrix[0]->get(3, 1); //
-            cout << vec_matrix[0]->get(3, 2); //
-            cout << vec_matrix[0]->get(3, 3); //
-            cout << vec_matrix[0]->get(3, 4); //
+            int x; // variavel com nome fodase,sera mudada depois
+            cout << "\nVoce deseja imprimir somente uma matriz ou todas?\n";
+            cout << "1. Imprima somente uma.\n";
+            cout << "2. Imprima todas as matrizes disponiveis.\n";
+            cout << "3. Voltar para o menu.\n";
+            cout << "digite sua resposta: ";
+            cin >> x;
+
+            if(x == 1) {
+               int escolha;
+               cout << "Qual matriz voce deseja imprimir? (escolha por ordem de criacao)\n";
+               cout << "Matriz: ";
+               cin >> escolha;
+
+               if(escolha >= 0 && escolha <= vec_matrix.size()) {
+                  // Escolha é diminuida em 1 para entrar nos padroes de indice do vector.
+                  escolha--; 
+                  std::cout << "\n";
+                  vec_matrix[escolha]->print();
+               } else {
+                  std::cout << "*Matriz invalida ou inexistente.*\n";
+               }
+               break;
+            } else if(x == 2) { 
+               std::cout << "\n";
+               for (auto &&matrix : vec_matrix) {
+                  matrix->print();
+                  std::cout << "\n";
+               }
+               break;
+            } else 
+               break;
          }
       }
    }
